8bdb7c6e4d1958f0c5c83e93fe2972fdeca3450c
enesh-lbtm <eburyeva@gmail.com>
2021-08-04 11:17:32 +0300

    Update

--- src/com/drob5/Calculate.java
+++ src/com/drob5/Calculate.java
@@ -2,7 +2,7 @@ package com.drob5;
 
 public class Calculate {
     public void findDrob(int m, int n){
-        Integer rez = m/n;
+        Integerez = m/n;
         System.out.println("Целая часть: "+rez);
         Integer rez1 = (m % n);
         System.out.println("Дробная часть: "+rez1);
